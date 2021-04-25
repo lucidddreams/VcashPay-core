e6ac9f6943bc4f9b62577ae177a40223449aea6c
Sandoche Adittane <sandoche@protonmail.com>
2021-04-25 16:03:54 +0100

    :sparkles: FIxing wallet

--- win-release-package.sh
+++ win-release-package.sh
@@ -11,7 +11,7 @@ echo PACKAGE="${PACKAGE}"
 CHANGELOG=nxt-clone-client-${VERSION}.changelog.txt
 OBFUSCATE=$2
 
-FILES="changelogs conf html lib resource contrib logs"
+FILES="changelogs conf html lib resource contrib logs nxt_db"
 FILES="${FILES} ${APPLICATION}.exe ${APPLICATION}service.exe"
 FILES="${FILES} 3RD-PARTY-LICENSES.txt AUTHORS.txt LICENSE.txt"
 FILES="${FILES} DEVELOPERS-GUIDE.md OPERATORS-GUIDE.md README.md README.txt USERS-GUIDE.md"
