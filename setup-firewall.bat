2542305661742b6d70598c972d01f5841a6c545c
Hippolyte <>
2021-06-04 11:56:18 +0100

    update installer/setup.xml & setup-firewall.bat

--- setup-firewall.bat
+++ setup-firewall.bat
@@ -7,13 +7,15 @@
 set argC=0
 for %%x in (%*) do Set /A argC+=1
 
+
 if  %argC%==3 (echo OK) else (exit 1)
+if %2=="" (exit 1)
+if %3=="" (exit 1)
 
 netsh advfirewall firewall add rule name="VcashPayAllow" dir=in action=allow protocol=TCP localport=16874
 netsh advfirewall firewall add rule name="VcashPayAllow2" dir=in action=allow protocol=TCP localport=16876
 
-echo nxt.apiServerHost=%1 >> %3\VCASHPAY\conf\nxt.propreties  
-echo nxt.allowedBotHosts=* >> %3\vcashpay\conf\nxt.propreties
-echo nxt.adminPassword=%2 >> %3\vcashpay\conf\nxt.propreties
-echo nxt.myAddress=%1 >> %3\vcashpay\conf\nxt.propreties 
-
+echo nxt.apiServerHost=%2 >> %1  
+echo nxt.allowedBotHosts=* >> %1
+echo nxt.adminPassword=%3 >> %1
+echo nxt.myAddress=%2 >> %1
