38c8d0232e6ca377a6198bc5315afb4964da1409
Hippolyte <>
2021-06-07 08:51:38 +0100

    back

--- setup-firewall.bat
+++ setup-firewall.bat
@@ -1,18 +1,18 @@
-set argC=0
-for %%x in (%*) do Set /A argC+=1
-
-
-if  %argC%==3 (echo OK) else (exit 1)
-if %2=="" (exit 1)
-if %3=="" (exit 1)
-
 netsh advfirewall firewall add rule name="VcashPayAllow" dir=in action=allow protocol=TCP localport=16874
 netsh advfirewall firewall add rule name="VcashPayAllow2" dir=in action=allow protocol=TCP localport=16876
 
-mkdir %APPDATA%\VCASHPAY\
-mkdir %APPDATA%\VCASHPAY\conf\
+@REM set argC=0
+@REM for %%x in (%*) do Set /A argC+=1
 
-echo nxt.apiServerHost=%1 >> %APPDATA%\VCASHPAY\conf\nxt.properties
-echo nxt.allowedBotHosts=* >> %APPDATA%\VCASHPAY\conf\nxt.properties
-echo nxt.adminPassword=%2 >> %APPDATA%\VCASHPAY\conf\nxt.properties
-echo nxt.myAddress=%1 >> %APPDATA%\VCASHPAY\conf\nxt.properties
+
+@REM if  %argC%==3 (echo OK) else (exit 1)
+@REM if %2=="" (exit 1)
+@REM if %3=="" (exit 1)
+
+@REM mkdir %APPDATA%\VCASHPAY\
+@REM mkdir %APPDATA%\VCASHPAY\conf\
+
+@REM echo nxt.apiServerHost=%1 >> %APPDATA%\VCASHPAY\conf\nxt.properties
+@REM echo nxt.allowedBotHosts=* >> %APPDATA%\VCASHPAY\conf\nxt.properties
+@REM echo nxt.adminPassword=%2 >> %APPDATA%\VCASHPAY\conf\nxt.properties
+@REM echo nxt.myAddress=%1 >> %APPDATA%\VCASHPAY\conf\nxt.properties
